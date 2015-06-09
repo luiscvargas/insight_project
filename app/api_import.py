51e3523a65b00550d39990912a370209f67ea0ba
Luis C. Vargas <lcv1984@gmail.com>
2015-06-08 20:52:43 -0400

    get home data for single zip code (python notebook)
    needs to be implemented in api_import module.

--- app/api_import.py
+++ app/api_import.py
@@ -7,11 +7,27 @@ return the variables of interest for the project.
 Idea is to combine data by zipcode, so each returned
 dict/data frame should have a dict column. 
 
-can then save data in mysql/sqlite3 database """
+can then save data in mysql/sqlite3 database
+
+note zipcode must be entered as a five letter string, 
+not as number"""
+
+#import modules to handle requests to APIs
+import requests
+from lxml import html
 
 def query_zillow(zipcode):
 	#get housing information in zipcode
-	return zillow_data
+	zillow_string = "http://www.zillow.com/webservice/GetDemographics.htm?zws-id="
+	with open("../oauth_keys/zillow.keys") as f:
+		zillow_key = f.read().strip("\n")
+	zillow_url = zillow_string + zillow_key + "&zip=" + zipcode
+	r = requests.get(zillow_url)
+	if r.status_code != 200:
+		return -1
+	return r
+	#print r.text
+	#return zillow_data
 
 def query_census(zipcode):
 	#use tract-zipcode mapping here to filter for
