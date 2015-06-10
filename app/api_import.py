19ebbba19a928d4543492c34c03cb9843164a9b1
Luis C. Vargas <lcv1984@gmail.com>
2015-06-09 21:18:34 -0400

    Add functions for querying US Census API and extracting features from this data.

--- app/api_import.py
+++ app/api_import.py
@@ -16,14 +16,15 @@ not as number"""
 import requests
 from lxml import html
 import numpy as np 
+import pandas as pd
 
 def query_zillow(zipcode):
 
 	"""
-		Inputs: 
-			zipcode (string)
-		Outputs:
-			Zillow Home Value Index, Median Sale Price for given zipcode
+	Inputs: 
+		- zipcode (string)
+	Outputs:
+		- Zillow Home Value Index, Median Sale Price for given zipcode
 	"""
 	
 	zillow_string = "http://www.zillow.com/webservice/GetDemographics.htm?zws-id="
@@ -57,10 +58,94 @@ def query_zillow(zipcode):
 	
 	return value_index, value_median
 
-def query_census(zipcode):
+def format_census_request(list_keys,county,state='36'):
+
+	"""
+	Inputs: 
+		- list_keys (list of strings) - Variables to be imported via API call
+		- county (str) - One of counties in NY State, must be single value 
+			so list of tracts is unique
+		- state (str) - Currently only enabled for NY  
+	Outputs:
+		- url (str) - address for API call to US Census 5 Year ACS Profile Data
+	"""
+
+	if state != "36":
+		raise ValueError("state must be '36'")
+
+	tract = "*"  #get all tracts within county and state
+
+	with open("../oauth_keys/census.keys") as f:
+		census_key = f.read().strip("\n")
+
+	url_root = "http://api.census.gov/data/2013/acs5/profile?get="
+	url_vars = ",".join(list_keys)
+	url_reg  = "&for=tract:" + tract + "&in=state:" + state + "+county:" + county + "&key=" + census_key
+	url = url_root + url_vars + url_reg
+	
+	return url
+
+def query_census(census_vars_keys, county_code, zipcode):
 	#use tract-zipcode mapping here to filter for
 	#data in requested zipcode.
-	return census_data
+
+	county_list = {"05": "Bronx", "81": "Queens", "61": "Manhattan", 
+               "47": "Brooklyn", "85": "Staten Island"}
+    
+    #Request data for a given county
+    r = requests.get(format_request(census_vars_keys,county_code))
+
+    census_data = r.json()
+    census_df = pd.DataFrame(census_data)
+	census_df.columns = census_df.iloc[0]
+	census_df = census_df[1:].reset_index()
+	return census_df
+
+def get_census_features(df,zipcode):
+	dfsub = df[df['zipcode'] == zipcode]
+	
+	number_households = dfsub['DP03_0051E'].astype('float').sum()
+	
+	n_households = np.array([dfsub['DP03_0052E'].astype('float').sum(),  # < 10k
+			dfsub['DP03_0053E'].astype('float').sum(),  # 10,000 to 14,999
+			dfsub['DP03_0054E'].astype('float').sum(),  # 15,000 to 24,999
+			dfsub['DP03_0055E'].astype('float').sum(),  # 25,000 to 34,999
+			dfsub['DP03_0056E'].astype('float').sum(),  # 35,000 to 49,999
+			dfsub['DP03_0057E'].astype('float').sum(),  # 50,000 to 74,999
+			dfsub['DP03_0058E'].astype('float').sum(),  # 75,000 to 99,999
+			dfsub['DP03_0059E'].astype('float').sum()]) # 100,000 to 149,999
+	
+	income = np.array([10.,12.5,20.,30.,42.5,62.5,87.5,125.0]) * 1000.
+	average_income = np.sum(n_households * income) / np.sum(n_households)                  
+	total_population = dfsub['DP05_0001E'].astype('float').sum()
+	
+	#something is wrong with this variable - get same values as for total pop
+	latino_population = dfsub['DP05_0065E'].astype('float').sum()
+	
+	return number_households, average_income, total_population, latino_population
+
+def tract_to_zip():
+
+	"""
+	Inputs:
+		- None
+	Outputs: 
+		- (county, tract) - > (zipcode) dictionary
+	Note:
+		- Currently only for NY State, remove state = 36 flag for others
+	"""
+
+	df = pd.read_csv('../data/zcta_tract_rel_10.txt',dtype=str)
+
+	#df = df[(data['STATE'] == '36') & (data['COUNTY'] == '061')].reset_index()
+	df = df[(data['STATE'] == '36')].reset_index()
+
+	tract_zip_dict = {}
+
+	for index, row in df.iterrows():
+		tract_zip_dict[(row['COUNTY'],row['TRACT'])] = row['ZCTA5']
+	
+	return tract_zip_dict
 
 def query_yelp(zipcode,business_type):
 	#get number of restaurants, average rating, ...
