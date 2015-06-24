c0f320d7e2b03c6ece72954d6384caa08736b4f8
Luis C. Vargas <lcv1984@gmail.com>
2015-06-24 01:28:24 -0400

    add page for additional info on given zipcode

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import render_template, request
 from app import app
 import numpy as np
@@ -6,6 +8,17 @@ import string
 import pandas as pd
 #import pymysql as mdb
 
+@app.route('/zipcode',methods=["GET"])
+def zipcode_box():
+	zipcode = request.args.getlist("zipval")
+
+	df = pd.read_json("app/static/dat/underserved_time_data.json")
+ 
+	df = df.loc[float(zipcode[0]),:]  #float str () to remove unicode and match floating type
+
+	borough = df.BOROUGH
+
+	return render_template("zipcode.html",zip=str(zipcode[0]),borough=borough)
 
 @app.route('/')
 @app.route('/index')
@@ -52,12 +65,13 @@ def restaurants_output():
 	#	return render_template("output.html",areas=[],cuisine=cuisine_types)
 
 
-	df = pd.read_json("app/static/dat/underserved_data.json")
+	df = pd.read_json("app/static/dat/underserved_time_data.json")
 
 	dx_linear = "dx_linear_"+cuisine_type
 	dx_rf = "dx_rf_"+cuisine_type
 	feature_cuisine = "number_restaurants_"+cuisine_type
 	feature_cuisine_capita = "number_restaurants_capita_"+cuisine_type
+	dx_composite = "composite_score_" + cuisine_type
 
 	#restrict to areas within boroughs specified
 	if boroughs != []:
@@ -70,14 +84,19 @@ def restaurants_output():
 	dfsub = df.sort(columns=dx_linear, axis=0, 
         ascending=True, inplace=False, kind='quicksort', 
         na_position='last').head(NUMBER_ZIPCODES)[["BOROUGH","pop_total",
-		feature_cuisine,feature_cuisine_capita,"dx_linear_"+cuisine_type]]
+		feature_cuisine,feature_cuisine_capita,dx_linear,dx_rf,dx_composite,"dNdt","dNdt_scaled"]]
 
 	boroughs = dfsub.BOROUGH.tolist()
-	deviation = dfsub["dx_linear_"+cuisine_type]
-	deviations = ["{0:<5.1f}".format(-1.*x).strip() for x in dfsub["dx_linear_"+cuisine_type].tolist()]
+	#deviation = dfsub["dx_linear_"+cuisine_type]
+	score_linear = ["{0:<5.1f}".format(-1.*x).strip() for x in dfsub[dx_linear].tolist()]
+	score_rf = ["{0:<5.1f}".format(-1.*x).strip() for x in dfsub[dx_rf].tolist()]
+	score_composite = ["{0:<5.1f}".format(-1.*x).strip() for x in dfsub[dx_composite].tolist()]
+	dNdt = ["{0:<5.1f}".format(-1.*x).strip() for x in dfsub["dNdt"].tolist()]
+	dNdt_scaled = ["{0:<5.1f}".format(-1.*x).strip() for x in dfsub["dNdt_scaled"].tolist()]
+
 	zipcodes = [str(int(x)) for x in dfsub.index.values.tolist()]
 
-	areas = zip(zipcodes,boroughs,deviations)
+	areas = zip(zipcodes,boroughs,score_linear,score_rf,score_composite,dNdt,dNdt_scaled)
 
 	#call a function from a_Model package, note we are only pulling one result in the query
 
